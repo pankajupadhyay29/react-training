febccb3aeeb73505ac82a6ed4707c8da457d0abb
pankajupadhyay29 <pankajupadhyay29@gmail.com>
2020-11-29 16:37:36 +0000

    Typescript examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Student, Test } from './ts_test';
 
 function App() {
+  const obj = new Test();
+  obj.setValue(10);
+
+  const student = new Student('FName', 'LName', 20);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={Test.varTest}>var Test</button>
+        <button onClick={Test.letTest}>let Test</button>
+        <button onClick={obj.getValue} >this without arrow Test</button>
+        <button onClick={() => obj.getValue()} >this with arrow Test</button>
+        <button onClick={() => alert(student.Name)}>class Test</button>
       </header>
     </div>
   );
---
+++ src/ts_test.ts
@@ -0,0 +1,57 @@
+export class Test {
+  public static varTest() {
+    for (var i = 0; i <= 5; i++) {
+      setTimeout(() => console.log(i));
+    }
+  }
+
+  public static letTest() {
+    for (let i = 0; i <= 5; i++) {
+      setTimeout(() => console.log(i));
+    }
+  }
+
+  private value: number = 0;
+
+  public getValue() {
+    console.log(this.value);
+    return this.value;
+  }
+
+  public setValue(value: number) {
+    this.value = value;
+  }
+}
+
+interface IStudent {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+interface IKeyValuePair {
+    [subject: string]: number; 
+}
+
+export class Student implements IStudent {
+  public firstName: string;
+  public lastName: string;
+  public age: number;
+  public marks: IKeyValuePair = {};
+
+  constructor(firstName: string, lastName: string, age: number) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  public get Name() {
+    const { firstName, lastName } = this;
+    return `${firstName} ${lastName}`;
+  }
+
+  public addMarks(subject: string, marks: number) {
+    this.marks[subject] = marks;
+  }
+  
+}
