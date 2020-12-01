fc2fded7d080e7b54cfb308d046746fd9c63f12b
Pankaj Upadhyay <pankaj.upadhyay@globallogic.com>
2020-12-01 21:56:05 +0000

    class component and state

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 import Bug from "./Bugs/Bug";
 
-const bugs = [
+interface IBug {
+	id: string;
+	description: string;
+	createdBy: string;
+	assignedTo: string;
+	status: string; // "Open" | "On Hold" | "In Progress" | "Resolved" | "Closed"
+}
+
+const initialBugList: IBug[] = [
 	{
 		id: "HR-121212",
 		description: "This is a test HR issue",
@@ -32,66 +40,117 @@ const bugs = [
 		status: "In Progress",
 	},
 ];
-function App() {
-	return (
-		<div className="flex-column">
-			<header>
-				<div className="flex-row navbar">
-					<a href="#">Dashboard</a>
-					<a href="#">Assigned to Me</a>
-					<a href="#">Reported By Me</a>
-					<a href="#">Search</a>
-					<a href="#">Create</a>
-				</div>
-			</header>
-			<div>
-				<h1>Bugs assigned to you</h1>
-				<ul className="list">
-					<li className="flex-row">
-						<div>Key</div>
-						<div>Summary</div>
-						<div>Assigned To</div>
-						<div>Status</div>
-					</li>
-					{bugs.map((bug) => {
-            return <Bug key={bug.id} {...bug}></Bug>
-						{/*<li key={bug.id} className="flex-row">
-							<div>{bug.id}</div>
-							<div>{bug.description}</div>
-							<div>{bug.assignedTo}</div>
-							<div>{bug.status}</div>
-          </li>*/}
-        })}
-					{/*
-					<li className="flex-row">
-						<div>HR-121212</div>
-						<div>This is a test HR issue</div>
-						<div>pankaj.upadhyay</div>
-						<div>Open</div>
-					</li>
-					<li className="flex-row">
-						<div>FN-121314</div>
-						<div>This is a test Finance issue</div>
-						<div>pankaj.upadhyay</div>
-						<div>In Progress</div>
-					</li>
-					<li className="flex-row">
-						<div>TR-121315</div>
-						<div>This is a test Travel issue</div>
-						<div>pankaj.upadhyay</div>
-						<div>In Progress</div>
-					</li>
-					<li className="flex-row">
-						<div>IT-121316</div>
-						<div>This is a test IT issue</div>
-						<div>pankaj.upadhyay</div>
-						<div>In Progress</div>
-					</li>
-        */}
-				</ul>
-			</div>
-		</div>
-	);
-}
 
-export default App;
+type AppProps = {};
+type AppsState = { bugs: IBug[] };
+export default class App extends React.Component<AppProps, AppsState> {
+	constructor(props: AppProps) {
+		super(props);
+		this.state = {
+			bugs: initialBugList,
+		};
+	}
+
+	public render() {
+		const { bugs } = this.state;
+		return (
+			<div className="flex-column">
+				<header>
+					<div className="flex-row navbar">
+						<a href="#">Dashboard</a>
+						<a href="#">Assigned to Me</a>
+						<a href="#">Reported By Me</a>
+						<a href="#">Search</a>
+						<a href="#">Create</a>
+					</div>
+				</header>
+				<div>
+					<h1>Bugs assigned to you</h1>
+					<ul className="list">
+						<li className="flex-row">
+							<div>Key</div>
+							<div>Summary</div>
+							<div>Assigned To</div>
+							<div>Status</div>
+						</li>
+						{bugs.map((bug) => (
+							<Bug key={bug.id} {...bug} onChangeStatus={this.onChange}></Bug>
+						))}
+					</ul>
+				</div>
+			</div>
+		);
+	}
+
+	private onChange = (id: string, status: string) => {
+		const { bugs } = this.state;
+		const changedBug = bugs.find((bug) => bug.id === id);
+		if (changedBug) {
+			const updatedBugs = bugs.filter((bug) => bug.id !== id);
+			this.setState({ bugs: [...updatedBugs, { ...changedBug, status }] });
+		}
+	};
+}
+// function App() {
+// 	return (
+// 		<div className="flex-column">
+// 			<header>
+// 				<div className="flex-row navbar">
+// 					<a href="#">Dashboard</a>
+// 					<a href="#">Assigned to Me</a>
+// 					<a href="#">Reported By Me</a>
+// 					<a href="#">Search</a>
+// 					<a href="#">Create</a>
+// 				</div>
+// 			</header>
+// 			<div>
+// 				<h1>Bugs assigned to you</h1>
+// 				<ul className="list">
+// 					<li className="flex-row">
+// 						<div>Key</div>
+// 						<div>Summary</div>
+// 						<div>Assigned To</div>
+// 						<div>Status</div>
+// 					</li>
+// 					{bugs.map((bug) => {
+//             return <Bug key={bug.id} {...bug}></Bug>
+// 						{/*<li key={bug.id} className="flex-row">
+// 							<div>{bug.id}</div>
+// 							<div>{bug.description}</div>
+// 							<div>{bug.assignedTo}</div>
+// 							<div>{bug.status}</div>
+//           </li>*/}
+//         })}
+// 					{/*
+// 					<li className="flex-row">
+// 						<div>HR-121212</div>
+// 						<div>This is a test HR issue</div>
+// 						<div>pankaj.upadhyay</div>
+// 						<div>Open</div>
+// 					</li>
+// 					<li className="flex-row">
+// 						<div>FN-121314</div>
+// 						<div>This is a test Finance issue</div>
+// 						<div>pankaj.upadhyay</div>
+// 						<div>In Progress</div>
+// 					</li>
+// 					<li className="flex-row">
+// 						<div>TR-121315</div>
+// 						<div>This is a test Travel issue</div>
+// 						<div>pankaj.upadhyay</div>
+// 						<div>In Progress</div>
+// 					</li>
+// 					<li className="flex-row">
+// 						<div>IT-121316</div>
+// 						<div>This is a test IT issue</div>
+// 						<div>pankaj.upadhyay</div>
+// 						<div>In Progress</div>
+// 					</li>
+//         */}
+// 				</ul>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export default App;
--- src/Bugs/Bug.tsx
+++ src/Bugs/Bug.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React from "react";
 
 interface IBugProps {
-    id: string;
-    description: string;
-    assignedTo: string;
+	id: string;
+	description: string;
+	assignedTo: string;
     status: string;
+    onChangeStatus(id: string,  status: string): void;
 }
 
 function Bug(props: IBugProps) {
-    const { id, description, assignedTo, status } = props;
-    return <li className="flex-row">
-        <div>{id}</div>
-        <div>{description}</div>
-        <div>{assignedTo}</div>
-        <div>{status}</div>
-    </li>
+	const { id, description, assignedTo, status } = props;
+	return (
+		<li className="flex-row">
+			<div>{id}</div>
+			<div>{description}</div>
+			<div>{assignedTo}</div>
+			<div>
+				<select value={status} onChange={(event) => props.onChangeStatus(id, event.target.value)}>
+					<option value="Open">Open</option>
+					<option value="In Progress">In Progress</option>
+					<option value="On Hold">On Hold</option>
+					<option value="Resolved">Resolved</option>
+					<option value="Closed">Closed</option>
+				</select>
+			</div>
+		</li>
+	);
 }
 
 export default Bug;
